eed22babd053b3ddbc150bb552677b09ccc93765
lisernia <lisernia@gmail.com>
2021-08-03 11:37:41 -0300

    Delete src directory

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-// Main Component
-import App from './components/App';
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
