889c255a536f219bb2aee9a5ac40dabdf6d2016e
lisernia <lisernia@gmail.com>
2021-08-03 12:04:20 -0300

    Add files via upload

---
+++ src/client/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// Bootstrap components
+import Container from 'react-bootstrap/Container';
+// Pages
+import Home from '../pages/Home';
+
+
+function App() {
+	return (
+		
+		<Home />
+	);
+}
+
+export default App;
